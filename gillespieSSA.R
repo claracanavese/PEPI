bdd8580f576a91489da130fbf172fd25d96db7f1
claracanavese <claracanavese@gmail.com>
2023-07-24 17:17:37 +0200

    Update gillespieSSA.R

---
+++ gillespieSSA.R
@@ -0,0 +1,56 @@
+library("GillespieSSA2")
+
+# parameters
+params <- c(am=1.5, bm=0.0, ap=1.2, bp=0.0, om=0.01, op=0.001)
+final_time <- 3
+sim_name <- "Switching Process"
+# initial state
+initial_state <- c(zm=1000, zp=100)
+# reactions
+reactions <- list(
+  reaction("am*zm", c(zm = +1), name = "birth_minus"),
+  reaction("bm*zm", c(zm = -1), name = "death_minus"),
+  reaction("ap*zp", c(zp = +1), name = "birth_plus"),
+  reaction("bp*zp", c(zp = -1), name = "death_plus"),
+  reaction("op*zm", c(zp = +1), name = "switch_to_plus"),
+  reaction("om*zp", c(zm = +1), name = "switch_to_minus")
+)
+
+# exact method
+set.seed(1)
+out <- ssa(
+  initial_state = initial_state,
+  reactions = reactions,
+  params = params,
+  final_time = final_time,
+  method = ssa_exact(),
+  sim_name = sim_name, 
+  log_propensity = TRUE,
+  log_firings = TRUE
+)
+plot_ssa(out)
+print(out$stats)
+print(out$state)
+print(out$time)
+propensity <- out$propensity
+firings <- as.data.frame(cbind(out$time,out$firings))
+colnames(firings)[1] <- "time"
+sum(firings$switch_to_plus)
+sum(firings$switch_to_minus)
+first_plus = filter(firings,switch_to_plus == 1)[1,]$time
+first_minus = filter(firings,switch_to_minus == 1)[1,]$time
+
+# explicit tau-leap
+set.seed(1)
+out <- ssa(
+  initial_state = initial_state,
+  reactions = reactions,
+  params = params,
+  final_time = final_time,
+  method = ssa_etl(tau = 0.01),
+  sim_name = sim_name
+)
+plot_ssa(out)
+
+saveRDS(out, file = "./simulations/gillespieSSA/out.Rdata")
+out$args
