615773ae954bf66d81cbffd322317a1b1e77732c
claracanavese <claracanavese@gmail.com>
2023-08-25 11:37:32 +0200

    Update fit.R

--- fit.R
+++ fit.R
@@ -4,14 +4,16 @@ library(dplyr)
 library(ggplot2)
 library(bayesplot)
 library(StanHeaders)
+library(easypar)
 
-model <- rstan::stan_model("./PEPI/regressionODE.stan")
+# model <- rstan::stan_model("./PEPI/regressionODE.stan")
 
 stan_fit = function(i){
   
-  model <- rstan::stan_model("./PEPI/regressionODE.stan")
-  simulation <- readRDS(paste0("./R/simulations/simulation_",i,".rds")) %>% tibble::as_tibble()
-  samples <- simulation[seq(12,82, by = 7),]
+  model <- rstan::stan_model("./regressionODE.stan")
+  simulation <- readRDS(paste0("./simulations/simulation_",i,".rds")) %>% tibble::as_tibble()
+  print(paste0("simulation_",i))
+  samples <- simulation[seq(12,82, by = 10),]
   data_list <- list(
     n_times = nrow(samples),
     z0 = c(1000,100,0,0,0),
@@ -21,17 +23,17 @@ stan_fit = function(i){
     t = samples$time
   )
   fit <- rstan::sampling(model, data_list, chains=4, warmup=6000, iter=12000, cores=4)
-  saveRDS(fit,paste0("./R/fit/fit_",i,".rds"))
+  saveRDS(fit,paste0("./fit/fit_",i,".rds"))
 
 }
 
-stan_fit(1)
-
-fit1 = readRDS("./R/fit/fit_1.rds")
-print(fit1, pars = c("lambda_minus", "lambda_plus", "omega_minus", "omega_plus"), digits_summary = 3)
+# stan_fit(1)
+# 
+# fit1 = readRDS("./fit/fit_1.rds")
+# print(fit1, pars = c("lambda_minus", "lambda_plus", "omega_minus", "omega_plus"), digits_summary = 3)
 
 easypar::run( FUN = stan_fit,
-              PARAMS = lapply(1:3, list),
+              PARAMS = lapply(1:900, list),
               parallel = TRUE,
               filter_errors = FALSE
 )
